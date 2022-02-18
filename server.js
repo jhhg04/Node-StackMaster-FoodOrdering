a0660337bc90fd3e3b1e78ea9e0c95a3522ed44e
jhhg0 <jhherrera@ccss.sa.cr>
2022-02-18 16:34:12 -0500

    add video 3

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const db = require('./db');
 const Pizza = require('./models/pizzaModel');
 const app = express();
 const pizzasRoute = require('./routes/pizzasRoute');
+const userRoute = require('./routes/userRoute');
 
 app.use(express.json());
 
 app.use('/api/pizzas', pizzasRoute);
+app.use('/api/users', userRoute);
 
 app.get('/', (req, res) => {
   res.send('Server Working');

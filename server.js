56f6a9e929b13ff5e1560c1d0d4ebc42a2f662be
jhhg0 <jhherrera@ccss.sa.cr>
2022-02-17 19:13:59 -0500

    add

--- server.js
+++ server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const db = require('./db');
 const Pizza = require('./models/pizzaModel');
 const app = express();
+const pizzasRoute = require('./routes/pizzasRoute');
 
 app.use(express.json());
+
+app.use('/api/pizzas', pizzasRoute);
 
 app.get('/', (req, res) => {
   res.send('Server Working');
 });
 
+/* 
 app.get('/getpizzas', (req, res) => {
   Pizza.find({}, (err, docs) => {
     res.send(docs);
@@ -19,6 +23,8 @@ app.get('/getpizzas', (req, res) => {
     }
   });
 });
+*/
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server Running on port ${port}`));
